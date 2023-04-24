43b9da029619a0e8b79a4f56ee59ac42b61fe647
Barys Smaliak <smboris@yandex.ru>
2023-04-24 18:12:04 +0300

    example001 added

---
+++ Example001/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа 
+// в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+int ReadNum (string argument)
+{
+    int num;
+    Console.WriteLine($"Input {argument}");
+    while (!int.TryParse(Console.ReadLine(), out num))
+    {
+        Console.WriteLine("It,s not a number");
+    }
+    return num;
+}
+
+void PrintNum(int n)
+{
+    if (n>=1) 
+    {     
+        Console.Write(n);
+        PrintNum(n-1); 
+    } 
+} 
+
+int N = ReadNum("N");
+PrintNum(N);
