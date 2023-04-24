c33f8d4f788e03f8e1ef0bdf774d40fd9cf0ee2a
Barys Smaliak <smboris@yandex.ru>
2023-04-24 19:55:09 +0300

    example002 added

---
+++ Example002/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int ReadNum (string argument)
+{
+    int num;
+    Console.WriteLine($"Input {argument}");
+    while (!int.TryParse(Console.ReadLine(), out num))
+    {
+        Console.WriteLine("It,s not a number");
+    }
+    return num;
+}
+
+(int m, int n) GreaterOfTwoNumbers (int m, int n)
+{
+    if (m > n)
+    {
+        int temp = m;
+        m = n;
+        n = temp;
+    }
+    return (m, n);
+}
+
+int sum =0;
+
+int PrintNum(int m, int n) 
+{      
+    if (m<=n) 
+    {     
+        sum += m;
+        PrintNum(m+1, n); 
+    } 
+    return sum;
+} 
+
+int M = ReadNum("M");
+int N = ReadNum("N");
+(int m, int n) = GreaterOfTwoNumbers(M, N);
+Console.Write(PrintNum(m, n));
